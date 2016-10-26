94e661eb07e74d27aaedc4ab9caae727a7224fdc
sandman <sandmanman@qq.com>
2016-10-26 18:16:58 +0800

    【进行中】点击加载更多

--- pages/list/list.js
+++ pages/list/list.js
@@ -12,61 +12,27 @@ Page({
         start: 0,
         title: '豆瓣电影',
         hidden: false,
-        display: 'none'
+        disabled: false,
+        loading: false,
+        display: 'none',
+        loadMoreBtnText: '加载更多'
     },
-    onLoad: function(start) {
+    onLoad: function() {
         /* 监听页面加载
          * 一个页面只会调用一次
          * 参数可以获取wx.navigateTo和wx.redirectTo及<navigator/>中的 query
          */
-
-        // 页面初始化 options为页面跳转所带来的参数
-
-        var start = this.data.start;
-
         var that = this;
-        var apiURL = api.topHead + '?start=' + start;
-        // https请求
-        wx.request({
-            url: apiURL,
-            header: {
-                'Content-Type': 'application/json'
-            },
-            success: function(res) {
-                // 收到开发者服务成功返回的回调函数，res = {data: '开发者服务器返回的内容'}
-                that.setData({
-                    list: res.data.subjects,
-                    title: res.data.title,
-                    hidden: true,
-                    display: 'block'
-                });
-                // console.log(res.data.subjects);
-                console.log(res.data.start);
-
-                wx.setNavigationBarTitle({
-                    title: res.data.title
-                });
-
-            },
-            fail: function() {
-                // 接口调用失败
-            },
-            complete: function() {
-                // 接口调用结束的回调函数（调用成功、失败都会执行）
-            }
-        });
+        requestData(that, this.data.start);
     },
     onReady: function() {
         /* 监听页面初次渲染完成
          * 一个页面只会调用一次，代表页面已经准备妥当，可以和视图层进行交互。
          * 对界面的设置如wx.setNavigationBarTitle请在onReady之后设置。
          */
-        var that = this;
         wx.setNavigationBarTitle({
-            title: that.data.title
+            title: this.data.title
         });
-
-        console.log('onReady');
     },
     onShow: function() {
         /* 监听页面显示
@@ -87,13 +53,21 @@ Page({
         // 监听用户下拉动作
     },
     loadMore: function() {
+        var that = this;
         // 点击加载更多
         if ( !(this.data.start === 240) ) {
-            this.data.start += 20;
-            console.log('点击加载更多--start='+this.data.start);
-
+            //this.data.start += 20;
+            //console.log('点击加载更多start='+this.data.start);
+            that.setData({
+                loading: true
+            });
+            requestData(that, that.data.start += 20);
         } else {
-            console.warn('最后一页了，按钮应该disabled');
+            that.setData({
+                disabled: true,
+                loadMoreBtnText: '加载完了'
+            });
+            console.warn('数据加载完了');
         }
 
     },
@@ -104,3 +78,41 @@ Page({
         });
     }
 });
+
+
+/*
+ * 请求数据
+*/
+var apiURL = api.topHead;
+function requestData(that, targetStart) {
+
+    wx.request({
+        url: apiURL + '?start=' + targetStart,
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+            // 收到开发者服务成功返回的回调函数，res = {data: '开发者服务器返回的内容'}
+            that.setData({
+                list: that.data.list.concat(res.data.subjects),
+                title: res.data.title,
+                hidden: true,
+                loading: false,
+                display: 'block'
+            });
+            // console.log(res.data.subjects);
+            if ( res.data.title != null ) {
+                wx.setNavigationBarTitle({
+                    title: res.data.title
+                });
+            }
+
+        },
+        fail: function() {
+            // 接口调用失败
+        },
+        complete: function() {
+            // 接口调用结束的回调函数（调用成功、失败都会执行）
+        }
+    });
+}
