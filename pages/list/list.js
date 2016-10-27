b064335b4895566f9d101a39256426ab5e32605c
sandman <sandmanman@qq.com>
2016-10-27 14:07:19 +0800

    优化

--- pages/list/list.js
+++ pages/list/list.js
@@ -17,13 +17,50 @@ Page({
         display: 'none',
         loadMoreBtnText: '加载更多'
     },
+    handleLoadMore: function(that, targetStart) {
+        /*
+         * 请求数据
+        */
+        var apiURL = api.topHead;
+        wx.request({
+            url: apiURL + '?start=' + targetStart,
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function(res) {
+                // 收到开发者服务成功返回的回调函数，res = {data: '开发者服务器返回的内容'}
+                that.setData({
+                    list: that.data.list.concat(res.data.subjects),
+                    title: res.data.title,
+                    hidden: true,
+                    loading: false,
+                    disabled: false,
+                    display: 'block'
+                });
+                // console.log(res.data.subjects);
+                if ( res.data.title !== null ) {
+                    wx.setNavigationBarTitle({
+                        title: res.data.title
+                    });
+                }
+
+            },
+            fail: function() {
+                // 接口调用失败
+                console.error('request fail:请求时间超时或...');
+            },
+            complete: function() {
+                // 接口调用结束的回调函数（调用成功、失败都会执行）
+            }
+        });
+    },
     onLoad: function() {
         /* 监听页面加载
          * 一个页面只会调用一次
          * 参数可以获取wx.navigateTo和wx.redirectTo及<navigator/>中的 query
          */
         var that = this;
-        requestData(that, this.data.start);
+        this.handleLoadMore(that, this.data.start);
     },
     onReady: function() {
         /* 监听页面初次渲染完成
@@ -52,17 +89,26 @@ Page({
     onPullDownRefresh: function() {
         // 监听用户下拉动作
     },
-    loadMore: function() {
-        var that = this;
+    movieSubject: function(event) {
+        // 跳转到电影详细页
+        //
+        var movieID = event.currentTarget.dataset.id,
+            movieTile = event.currentTarget.dataset.title;
+        wx.navigateTo({
+            url: '../detail/detail?id=' + movieID + '&title=' + movieTile
+        });
+    },
+    loadMoreTap: function() {
         // 点击加载更多
+        //
+        var that = this;
         if ( this.data.start !== 240 ) {
-            //this.data.start += 20;
-            //console.log('点击加载更多start='+this.data.start);
             that.setData({
                 loading: true,
                 disabled: true
             });
-            requestData(that, that.data.start += 20);
+
+            this.handleLoadMore(that, that.data.start += 20);
         } else {
             that.setData({
                 disabled: true,
@@ -70,7 +116,6 @@ Page({
             });
             console.warn('数据加载完了');
         }
-
     },
     loadingChange: function() {
         // loading
@@ -79,42 +124,3 @@ Page({
         });
     }
 });
-
-
-/*
- * 请求数据
-*/
-var apiURL = api.topHead;
-function requestData(that, targetStart) {
-
-    wx.request({
-        url: apiURL + '?start=' + targetStart,
-        header: {
-            'Content-Type': 'application/json'
-        },
-        success: function(res) {
-            // 收到开发者服务成功返回的回调函数，res = {data: '开发者服务器返回的内容'}
-            that.setData({
-                list: that.data.list.concat(res.data.subjects),
-                title: res.data.title,
-                hidden: true,
-                loading: false,
-                disabled: false,
-                display: 'block'
-            });
-            // console.log(res.data.subjects);
-            if ( res.data.title !== null ) {
-                wx.setNavigationBarTitle({
-                    title: res.data.title
-                });
-            }
-
-        },
-        fail: function() {
-            // 接口调用失败
-        },
-        complete: function() {
-            // 接口调用结束的回调函数（调用成功、失败都会执行）
-        }
-    });
-}
